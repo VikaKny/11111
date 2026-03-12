a0c37283373c22d4142999b27a704087bcdbca8c
VikaKny <viktoriia.yu.knyshuk@ukd.edu.ua>
2026-03-12 13:18:24 +0200

    add pr5 file

--- pr5.py
+++ pr5.py
@@ -3,3 +3,8 @@ print("Hello world")
 print("Hello from Boda")
 print("test")
 print("Another")
+human_age = int(input("Введіть ваш вік: "))
+
+dog_age = human_age * 7
+
+print("Ваш вік у собачих роках:", dog_age)
